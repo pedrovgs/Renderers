0cc68e0c4b7202d61c2406fff6b71b9fcb6aa3fb
pedrovgs <pedrovicente.gomez@gmail.com>
2014-02-24 21:22:05 +0100

    Create repository with basic structure

---
+++ sample/src/main/java/com/pedro/renderers/MainActivity.java
@@ -0,0 +1,20 @@
+package com.pedro.renderers;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Example activity created to show how to use Renderers.
+ *
+ * @author Pedro Vicente Gómez Sánchez
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
