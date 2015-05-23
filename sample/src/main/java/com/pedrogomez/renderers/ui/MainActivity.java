068b94d0ac87858e9d95d3f2df253f91d9058879
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-05-23 20:14:52 +0200

    Change MainActivity to show two buttons used to open two different samples

---
+++ sample/src/main/java/com/pedrogomez/renderers/ui/MainActivity.java
@@ -0,0 +1,35 @@
+package com.pedrogomez.renderers.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import com.pedrogomez.renderers.R;
+
+/**
+ * ListViewActivity for the Renderers demo.
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class MainActivity extends Activity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    ButterKnife.inject(this);
+  }
+
+  @OnClick(R.id.bt_open_lv_sample) public void openListViewSample() {
+    open(ListViewActivity.class);
+  }
+
+  @OnClick(R.id.bt_open_rv_sample) public void openRecyclerViewSample() {
+
+  }
+
+  private void open(Class activity) {
+    Intent intent = new Intent(this, activity);
+    startActivity(intent);
+  }
+}
